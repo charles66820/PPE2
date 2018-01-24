12345cc9606e8e8881edc4f64285430b65eabc5f
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-01-24 17:56:31 +0100

    Nav & img

---
+++ nav.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="css/imgNav.css">
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav">
+                <li class=""><a href="#"><img class="imgNav" src="./img/poulpeNav.png"></a></li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Figurines
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Pop</a>
+                    <a class="dropdown-item" href="#">Nenedoroid</a>
+                    <a class="dropdown-item" href="#">Officiel</a>
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Vêtement
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Homme</a>
+                    <a class="dropdown-item" href="#">Femme</a>
+                    <a class="dropdown-item" href="#">Enfant</a>
+                    <a class="dropdown-item" href="#">Cosplay</a>
+                    <a class="dropdown-item" href="#">Kigurumi</a>
+                    <a class="dropdown-item" href="#">Bijoux</a>
+                  </div>
+                </li>
+                <nav class="navbar navbar-light bg-faded">
+                  <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </nav>
+                <li style="float:right" class=""><a href="#"><img class="iconNav" src="./img/compteNav.png"></a></li>
+                <li style="float:right" class=""><a href="#"><img class="iconNav" src="./img/panierNav.png"></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
