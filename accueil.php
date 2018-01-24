e1a64c09a58a5831a0ca054e20caa3ba76788fc1
PaulinePro <paulinedelhaye6298@gmail.com>
2018-01-24 15:17:58 +0100

    Accueil de poulpe

--- accueil.php
+++ accueil.php
@@ -4,25 +4,24 @@
     <meta charset="UTF-8">
     <title>Accueil</title>
     <link rel="" href="" />
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/accueil.css">
+    <link rel="stylesheet" href="./style/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="./style/bootstrap.min.css">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/accueil.css">
 </head>
 <body>
 <!-- nav (kevin)  -->
 
 
-bla bla bla 
 
 <!-- pauline -->
 <div id="slider">
   <figure>
-    <img src="poulpe1.jpg" alt>
-    <img src="poulpe2.jpg" alt>
-    <img src="poulpe3.jpg" alt>
-    <img src="poulpe4.jpg" alt>
-    <img src="poulpe1.jpg" alt>
+    <img src="./img/poulpe1.jpg" alt>
+    <img src="./img/poulpe2.jpg" alt>
+    <img src="./img/poulpe3.jpg" alt>
+    <img src="./img/poulpe4.jpg" alt>
+    <img src="./img/poulpe1.jpg" alt>
   </figure>
 </div>
 
