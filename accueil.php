64e3a240224b97e5d29aa2cc0f64e2fe1db5ec5b
PaulinePro <paulinedelhaye6298@gmail.com>
2018-01-24 14:58:14 +0100

    accueil marche pas

--- accueil.php
+++ accueil.php
@@ -7,15 +7,24 @@
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/accueil.css">
 </head>
 <body>
 <!-- nav (kevin)  -->
 
 
-
+bla bla bla 
 
 <!-- pauline -->
-
+<div id="slider">
+  <figure>
+    <img src="poulpe1.jpg" alt>
+    <img src="poulpe2.jpg" alt>
+    <img src="poulpe3.jpg" alt>
+    <img src="poulpe4.jpg" alt>
+    <img src="poulpe1.jpg" alt>
+  </figure>
+</div>
 
 
 
