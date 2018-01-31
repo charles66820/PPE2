c1b01d22f175e4acbd192665536e6bfe04eb3505
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-01-31 11:16:44 +0100

    nav

--- nav.php
+++ nav.php
@@ -1,42 +1,43 @@
-<link rel="stylesheet" href="./style/imgNav.css">
-<link rel="stylesheet" href="./style/nav.min.css">
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navcol-1">
-            <ul class="nav navbar-nav">
-                <li class=""><a href="#"><img class="imgNav" src="./img/poulpeNav.png"></a></li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Figurines
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Pop</a>
-                    <a class="dropdown-item" href="#">Nenedoroid</a>
-                    <a class="dropdown-item" href="#">Officiel</a>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Vêtement
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Homme</a>
-                    <a class="dropdown-item" href="#">Femme</a>
-                    <a class="dropdown-item" href="#">Enfant</a>
-                    <a class="dropdown-item" href="#">Cosplay</a>
-                    <a class="dropdown-item" href="#">Kigurumi</a>
-                    <a class="dropdown-item" href="#">Bijoux</a>
-                  </div>
-                </li>
-                <nav class="navbar navbar-light bg-faded">
-                  <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                  </form>
-                </nav>
-                <li style="float:right" class=""><a href="#"><img class="iconNav" src="./img/compteNav.png"></a></li>
-                <li style="float:right" class=""><a href="#"><img class="iconNav" src="./img/panierNav.png"></a></li>
-            </ul>
+<head>
+  <link rel="stylesheet" href="./style/imgNav.css">
+</head>
+<div>
+    <nav class="navbar navbar-light navbar-expand-md">
+        <div class="container"><a class="navbar-brand" href="accueil.php"><img class="imgLogo" src="./img/logoPoulpe.png"/></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <form class="form-inline navbar-left">
+                    <div class="input-group"><span id="basic-addon1" class="input-group-addon"><i class="fa fa-search"></i></span><input class="form-control" type="text" placeholder="Search" aria-describedby="basic-addon1"></div>
+                </form>
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="accueil.php">Accueil</a></li>
+                    <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Figurines</a>
+                        <div class="dropdown-menu" role="menu">
+                          <a class="dropdown-item" role="presentation" href="#">Pop</a>
+                          <a class="dropdown-item" role="presentation" href="#">Nenedoroid</a>
+                          <a class="dropdown-item" role="presentation" href="#">Officiel</a>
+                        </div>
+                    </li>
+                    <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Vêtement</a>
+                        <div class="dropdown-menu" role="menu">
+                          <a class="dropdown-item" role="presentation" href="#">Homme</a>
+                          <a class="dropdown-item" role="presentation" href="#">Femme</a>
+                          <a class="dropdown-item" role="presentation" href="#">Enfant</a>
+                          <a class="dropdown-item" role="presentation" href="#">Cosplay</a>
+                          <a class="dropdown-item" role="presentation" href="#">Kigurumi</a>
+                          <a class="dropdown-item" role="presentation" href="#">Bijoux</a>
+                          <a class="dropdown-item" role="presentation" href="#">Sous-vêtements</a>
+                          <a class="dropdown-item" role="presentation" href="#">Bonnets & Casquettes</a>
+                        </div>
+                    </li>
+                    <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Compte</a>
+                        <div class="dropdown-menu" role="menu">
+                          <a class="dropdown-item" role="presentation" href="editionprofil.php">Identifiez vous ! </a>
+                          <a class="dropdown-item" role="presentation" href="inscription.php">Nouveau client !</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
