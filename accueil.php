2eff34121b03d0df184ee3ebc527c0e49a516848
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-02-01 15:15:26 +0100

    accueil

--- accueil.php
+++ accueil.php
@@ -9,10 +9,21 @@
 </head>
 <body>
   <?php include 'nav.php'; ?>
-<!-- nav (kevin)  -->
-
-
-
+  <div class="highlight-phone">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-8">
+                      <div class="intro">
+                          <h2 style="height:27px;color:#2a6bab;font-size:36px;"><span style="text-decoration: underline;">Bienvenue</span> </h2>
+                          <p style="height:40px;font-size:22px;">Hey ! Et bienvenue à Ô'Tako ! Je suis Ô'Tako ! Et je serai là pour guider ... </p>
+                          <p style="font-size:22px;height:58px;">Si tu es nouveau ! Viens vite t''inscrire ! Pour devenir un membre de la ... ! </p><a class="btn btn-primary" role="button" href="#" style="background-color:rgb(27,159,167);height:61px;margin:9px;width:317px;padding:20px;font-size:20px;">M'inscrire maintenant ! </a></div>
+                  </div>
+                  <div class="col-sm-4" style="background-image:url(&quot;assets/img/postal_pulpo_lindo_del_dibujo_animado_en_rosa-rab08a3c83fee4266ab88ca97e53546ba_vgbaq_8byvr_324.jpg&quot;);">
+                      <div class="d-none d-md-block iphone-mockup"></div>
+                  </div>
+              </div>
+          </div>
+      </div>
 <!-- pauline -->
 <div id="slider">
   <figure>
@@ -23,9 +34,33 @@
     <img src="./img/poulpe1.jpg" alt>
   </figure>
 </div>
-
-
-
+<footer>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 footer-navigation">
+                <h3><a href="#">O<span>'Tako </span></a></h3>
+                <p class="links"><a href="#">Home</a><strong> · </strong><a href="#">Blog</a><strong> · </strong><a href="#">Pricing</a><strong> · </strong><a href="#">About</a><strong> · </strong><a href="#">Faq</a><strong> · </strong><a href="#">Contact</a></p>
+                <p class="company-name">Company Name © 2015 </p>
+            </div>
+            <div class="col-sm-6 col-md-4 footer-contacts">
+                <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p><span class="new-line-span">Lycée Jean Lurcat </span> Perpignan, France</p>
+                </div>
+                <div><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p class="footer-center-info email text-left"> +33 123 456 78</p>
+                </div>
+                <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                    <p><a href="#" target="_blank">Ô'tako@tako.com</a></p>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="col-md-4 footer-about">
+                <h4>About the company</h4>
+                <p> Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+                </p>
+                <div class="social-links social-icons"></div>
+            </div>
+        </div>
+    </footer>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
 
