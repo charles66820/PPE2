7e90c87bf1d80ea7d26bdca00408fa384df1b773
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-02-01 15:41:22 +0100

    accueil beta 2.0

--- accueil.php
+++ accueil.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Accueil</title>
-    <link rel="icon" href="./img/logoIcon.gif"/>    
+    <link rel="icon" href="./img/logoIcon.gif"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
@@ -17,9 +17,10 @@
               <div class="row">
                   <div class="col-md-8">
                       <div class="intro">
-                          <h2 style="height:27px;color:#2a6bab;font-size:36px;"><span style="text-decoration: underline;">Bienvenue</span> </h2>
-                          <p style="height:40px;font-size:22px;">Hey ! Et bienvenue à Ô'Tako ! Je suis Ô'Tako ! Et je serai là pour guider ... </p>
-                          <p style="font-size:22px;height:58px;">Si tu es nouveau ! Viens vite t''inscrire ! Pour devenir un membre de la ... ! </p><a class="btn btn-primary" role="button" href="#" style="background-color:rgb(27,159,167);height:61px;margin:9px;width:317px;padding:20px;font-size:20px;">M'inscrire maintenant ! </a></div>
+                          <h2 style="height:50px;color:#2a6bab;font-size:36px;"><span style="text-decoration: underline;">Bienvenue</span> </h2>
+                          <p style="height:20px;font-size:22px;">Hey ! Et bienvenue à Ô'Tako ! Je suis Ô'Tako ! Et je serai là pour guider ... </p>
+                          <p style="font-size:22px;height:20px;">Si tu es nouveau ! Viens vite t''inscrire ! Pour devenir un membre de la ... ! </p>
+                          <a class="btn btn-primary" role="button" href="#" style="background-color:rgb(27,159,167);height:75px;margin:15px;width:300px;padding:20px;font-size:20px;">M'inscrire maintenant ! </a></div>
                   </div>
                   <div class="col-sm-4" style="background-image:url(&quot;assets/img/postal_pulpo_lindo_del_dibujo_animado_en_rosa-rab08a3c83fee4266ab88ca97e53546ba_vgbaq_8byvr_324.jpg&quot;);">
                       <div class="d-none d-md-block iphone-mockup"></div>
@@ -37,7 +38,8 @@
     <img src="./img/poulpe1.jpg" alt>
   </figure>
 </div>
-<footer>
+<footer>  <div class="highlight-phone">
+          <div class="container">
         <div class="row">
             <div class="col-sm-6 col-md-4 footer-navigation">
                 <h3><a href="#">O<span>'Tako </span></a></h3>
@@ -63,6 +65,8 @@
                 <div class="social-links social-icons"></div>
             </div>
         </div>
+      </div>
+  </div>
     </footer>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
