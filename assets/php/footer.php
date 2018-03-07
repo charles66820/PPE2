83d55576e631c33f7700b07c2522ef97fcb79dae
101197 <valat.estelle@gmail.com>
2018-03-07 15:45:01 +0100

    otako

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,7 +1,7 @@
 <footer style="background-color:rgb(23,50,77);">
     <div class="row">
         <div class="col-sm-6 col-md-4 footer-navigation">
-            <h3><a href="../accueil.php">Ô '<span>&nbsp;Tako</span></a></h3>
+            <h3><a style="font-size:60px;" href="../accueil.php">Ô '<span>&nbsp;Tako</span></a></h3>
             <p class="links"><a href="../accueil.php">Accueil</a><strong> ·
             </strong><a href="#">Sécurité</a><strong> ·
             </strong><a href="#">Livraison</a><strong> ·
