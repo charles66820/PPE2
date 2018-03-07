bab6c614906661a7c2325db696b3b92fba51eb9f
charles66820 <charles.goedefroit@gmail.com>
2018-03-07 08:34:54 +0100

    le javascripts!!!!!!!

--- ajouterproduit.php
+++ ajouterproduit.php
@@ -186,5 +186,6 @@
       ?>
     <script src="./assets/js/jquery-3.3.1.min.js"></script>
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script src="'assets/js/ajouterproduit.js'"></script>
   </body>
 </html>
