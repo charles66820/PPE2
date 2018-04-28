5a5d54a293be70f7ed5623ea18fe44443c424980
charles66820 <charles.goedefroit@gmail.com>
2018-04-28 19:35:51 +0200

    indentation est suite fonts erreur

--- accueil.php
+++ accueil.php
@@ -13,20 +13,20 @@
   <body>
     <?php include 'assets/php/nav.php'; ?>
     <div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8" style="color:rgb(9,73,119);">
-                    <div>
-                        <div class="intro">
-                            <h1 class="text-center" style="font-size:59px;margin-top:74px;"><i class="icon-ghost"></i>&nbsp;<strong><span style="text-decoration: underline;">Bienvenue</span></strong>&nbsp;<i class="icon-ghost"></i></h1>
-                            <p class="text-center" style="font-size:21px;width:none px;margin-top:21px;">Hey, bienvenue sur Ô'Tako !&nbsp;<br>Je suis Poulpi et je suis là pour te guider.&nbsp;<br>Si tu es nouveau, viens vite t'inscrire et&nbsp;<br>deviens un membre de la Tako Family !&nbsp;</p>
-                        </div>
-                        <div class="buttons"><a class="btn btn-info btn-lg d-block float-none justify-content-center align-items-center align-content-center align-self-center order-4 mx-auto" role="button" href="#" data-bs-hover-animate="bounce" style="width:435px;font-size:27px;max-width:466px;margin-left:0px;margin-top:34px;"><i class="fa fa-hand-o-right"></i>&nbsp;M'inscrire MAINTENANT !&nbsp;<i class="fa fa-hand-o-left"></i></a></div>
-                    </div>
-                </div>
-                <div class="col-md-4"><img class="d-block mx-auto" src="assets/img/logoPoulpe.png" style="width:259px;margin-top:88px;margin-left:40px;"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8" style="color:rgb(9,73,119);">
+            <div>
+              <div class="intro">
+                <h1 class="text-center" style="font-size:59px;margin-top:74px;"><i class="icon-ghost"></i>&nbsp;<strong><span style="text-decoration: underline;">Bienvenue</span></strong>&nbsp;<i class="icon-ghost"></i></h1>
+                <p class="text-center" style="font-size:21px;width:none px;margin-top:21px;">Hey, bienvenue sur Ô'Tako !&nbsp;<br>Je suis Poulpi et je suis là pour te guider.&nbsp;<br>Si tu es nouveau, viens vite t'inscrire et&nbsp;<br>deviens un membre de la Tako Family !&nbsp;</p>
+              </div>
+              <div class="buttons"><a class="btn btn-info btn-lg d-block float-none justify-content-center align-items-center align-content-center align-self-center order-4 mx-auto" role="button" href="#" data-bs-hover-animate="bounce" style="width:435px;font-size:27px;max-width:466px;margin-left:0px;margin-top:34px;"><i class="fa fa-hand-o-right"></i>&nbsp;M'inscrire MAINTENANT !&nbsp;<i class="fa fa-hand-o-left"></i></a></div>
             </div>
+          </div>
+          <div class="col-md-4"><img class="d-block mx-auto" src="assets/img/logoPoulpe.png" style="width:259px;margin-top:88px;margin-left:40px;"></div>
         </div>
+      </div>
     </div>
     <div id="slider" style="margin-top:70px;">
       <figure>
