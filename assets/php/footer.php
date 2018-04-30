19f75f1b02bd4257c075b5e4d722c7e26e3055f1
PaulinePro <paulinedelhaye6298@gmail.com>
2018-04-30 18:53:22 +0200

    footer

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -10,7 +10,7 @@
         </div>
         <div class="col-sm-6 col-md-4 footer-contacts">
             <div><span class="fa fa-map-marker footer-contacts-icon" style="background-color:rgb(21,107,159);"> </span>
-                <p><span class="new-line-span">21 Revolution Street</span> Perpignan, France</p>
+                <p><span class="new-line-span">8 rue des tentacules, Avenue des otakus, 66000 Octlantis World.</span></p>
             </div>
             <div><i class="fa fa-phone footer-contacts-icon" style="background-color:rgb(21,107,159);"></i>
                 <p class="footer-center-info email text-left"> +334 27 86 84 23</p>
@@ -21,8 +21,9 @@
         </div>
         <div class="clearfix"></div>
         <div class="col-md-4 footer-about">
-            <h4>Remerciement&nbsp;</h4>
-            <p> Nous vous remercions d'avoir visité notre site de vente. Nous espérons vous revoir très bientôt.&nbsp;</p>
+            <h4>Pour information :&nbsp;</h4>
+            <p> Octlantis est une ville sous-marine construite par des poulpes ! <br>
+                Plus d'infos ici --> <a href="https://www.lexpress.fr/actualite/sciences/voici-octlantis-une-ville-sous-marine-construite-par-des-poulpes_1944982.html">Octlantis</a> &nbsp;</p>
         </div>
     </div>
 </footer>
