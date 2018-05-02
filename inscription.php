9b973c50056d2b9b40e11a910468f6d928f3f851
Kevin66000 <kevin.le.lurcat@gmail.com>
2018-05-02 08:10:28 +0200

    ins vs 4

--- inscription.php
+++ inscription.php
@@ -97,108 +97,6 @@
 
     <!-- formulaire d'inscription -->
     <div align="center">
-<<<<<<< HEAD
-      <h1>Inscription</h1>
-      <br/>
-      <form method="POST" action="">
-        <table>
-          <!-- pseudo -->
-          <tr>
-            <td align="right">
-              <label for="pseudo">Pseudo : </label>
-            </td>
-            <td>
-              <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" /> <!--Permet de laisser affiché après validation si erreur-->
-            </td>
-          </tr>
-          <!-- mail -->
-          <tr>
-            <td align="right">
-              <label for="mail">Mail : </label>
-            </td>
-            <td>
-              <input type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right">
-              <label for="mail2">Confirmation du mail : </label>
-            </td>
-            <td>
-              <input type="email" placeholder="Confirmez votre mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) { echo $mail2; } ?>" />
-            </td>
-          </tr>
-
-          <!-- mdp -->
-          <tr>
-            <td align="right">
-              <label for="mdp">Mot de passe : </label>
-            </td>
-            <td>
-              <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right">
-              <label for="mdp2">Confirmation du mot de passe : </label>
-            </td>
-            <td>
-              <input type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" />
-            </td>
-          </tr>
-
-          <!-- Nom -->
-          <tr>
-            <td align="right">
-              <label for="nom">Nom : </label>
-            </td>
-            <td>
-              <input type="nom" placeholder="Votre nom" id="nom" name="nom" value="<?php if(isset($nom)) { echo $nom; } ?>" />
-            </td>
-          </tr>
-
-          <!-- Prénom -->
-          <tr>
-            <td align="right">
-              <label for="prenom">Prénom : </label>
-            </td>
-            <td>
-              <input type="prenom" placeholder="Votre prénom" id="prenom" name="prenom" value="<?php if(isset($prenom)) { echo $prenom; } ?>" />
-            </td>
-          </tr>
-
-          <!-- Téléphone -->
-          <tr>
-            <td align="right">
-              <label for="telephone">Téléphone : </label>
-            </td>
-            <td>
-              <input type="telephone" placeholder="Votre n° de téléphone" id="telephone" name="telephone" value="<?php if(isset($telephone)) { echo $telephone; } ?>" />
-            </td>
-          </tr>
-
-          <!--Civilité-->
-          <tr>
-            <td align="right">
-              <label>Civilité : </label>
-            </td>
-            <td>
-              <input type="radio" name="civilite" value="homme" checked>Monsieur<br>
-              <input type="radio" name="civilite" value="femme">Madame<br>
-
-            </td>
-          </tr>
-
-        </table>
-        <br />
-        <input type="submit" name="forminscription" value="Je m'inscris" />
-      </form>
-      <?php
-        if(isset($erreur)) {
-          echo '<font color="red">'.$erreur."</font>";
-        }
-      ?>
-=======
       <div class="container">
         <div class="card card-container" style="max-width: 350px; padding: 40px 40px;">
           <h1>Inscription</h1>
@@ -260,7 +158,6 @@
             }
           ?>
       </div>
->>>>>>> 4fd13dc37ec60ec2045e8a2ffbe9fc45f9350ee5
     </div>
   </div>
     <?php include 'assets/php/footer.php'; ?>
